79e8984461f63b0a06c9165f0354022e299a9c9d
ilker <ilker897@gmail.com>
2022-07-16 14:13:34 +0300

    tinder cards added

---
+++ src/components/PersonCard.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import {Animated, ImageSourcePropType, Pressable, StyleSheet} from 'react-native';
+import {Image, Text, View} from 'react-native-ui-lib';
+import {useConstants} from '../utils/constants';
+import {Shadows} from '../utils/designSystem';
+
+interface Props {
+  person: {
+    name: string;
+    age: string;
+    img: ImageSourcePropType;
+    desc: string;
+  };
+  swipe: any;
+  isFirst: boolean;
+  zIndex: number;
+}
+
+const PersonCard = ({person, swipe, isFirst, zIndex, ...rest}: Props) => {
+  const {dim} = useConstants();
+
+  const animatedPersonStyle = {
+    transform: [
+      ...swipe.getTranslateTransform(),
+      {
+        rotate: swipe.x.interpolate({
+          inputRange: [-100, 0, 100],
+          outputRange: ['8deg', '0deg', '-8deg'],
+        }),
+      },
+    ],
+  };
+
+  return (
+    <Animated.View {...rest} style={[styles.card, {width: dim.width - 48}, isFirst && animatedPersonStyle]}>
+      <View style={styles.cardContent}>
+        <View style={styles.cardImage}>
+          <Image source={person.img} style={styles.cardImage} resizeMode="contain" />
+        </View>
+        <View style={styles.cardText}>
+          <Text white small>
+            {person.name} , {person.age}
+          </Text>
+          <Text>{person.desc}</Text>
+        </View>
+      </View>
+    </Animated.View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    ...Shadows.dark,
+    borderRadius: 10,
+    height: 500,
+    alignSelf: 'center',
+    borderWidth: 1,
+    borderColor: 'black',
+    backgroundColor: '#fff',
+    position: 'absolute',
+  },
+  cardContent: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  cardImage: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
+  },
+  cardText: {
+    flex: 1,
+    padding: 10,
+  },
+});
+
+export default PersonCard;
--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
-import {Colors, Text, View} from 'react-native-ui-lib';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
+import {Dimensions, StyleSheet, Animated, PanResponder} from 'react-native';
+import {Colors, Image, Text, View} from 'react-native-ui-lib';
 import FoodCircle from '../components/FoodCircle';
+import PersonCard from '../components/PersonCard';
+import {useConstants} from '../utils/constants';
 
 const foods = [
   {
@@ -17,12 +20,103 @@ const foods = [
   },
 ];
 
+const peopleData = [
+  {
+    name: 'Ali',
+    age: '25',
+    img: require('../assets/images/person1.jpeg'),
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    name: 'Veli',
+    age: '25',
+    img: require('../assets/images/person2.jpeg'),
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    name: 'Mehmet',
+    age: '25',
+    img: require('../assets/images/person3.jpeg'),
+
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    name: 'Ali',
+    age: '25',
+    img: require('../assets/images/person4.jpeg'),
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+];
+
 const Main = () => {
+  const [people, setPeople] = useState(peopleData);
+  const {
+    dim: {width: SCREEN_WIDTH},
+  } = useConstants();
+
+  const swipe = useRef(new Animated.ValueXY()).current;
+
+  const removePerson = useCallback(() => {
+    setPeople(prev => {
+      return prev.slice(0, -1);
+    });
+    swipe.setValue({x: 0, y: 0});
+  }, [swipe]);
+
+  useEffect(() => {
+    if (people.length === 0) {
+      setPeople(peopleData);
+    }
+  }, [people]);
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: (event, {dx, dy}) => {
+      swipe.setValue({x: dx, y: dy});
+    },
+    onPanResponderRelease: (event, gestureState) => {
+      const direction = Math.sign(gestureState.dx);
+      console.log('dx', gestureState.dx);
+      if (gestureState.dx > 100 || gestureState.dx < -100) {
+        Animated.spring(swipe, {
+          toValue: {x: direction * (SCREEN_WIDTH + 100), y: 0},
+          useNativeDriver: true,
+        }).start(() => removePerson());
+      } else {
+        Animated.spring(swipe, {
+          toValue: {x: 0, y: 0},
+          friction: 5,
+          useNativeDriver: true,
+        }).start();
+      }
+    },
+  });
+
   const Circles = () => {
     return (
-      <View row spread paddingH-48>
+      <View row spread paddingH-36>
         {foods.map(food => {
           return <FoodCircle onPress={() => {}} img={food.img} key={food.text} text={food.text} />;
+        })}
+      </View>
+    );
+  };
+  const People = () => {
+    return (
+      <View centerH marginT-24>
+        {people.map((person, index) => {
+          const isFirst = index === people.length - 1;
+          const dragHandlers = isFirst ? panResponder.panHandlers : {};
+          return (
+            <PersonCard
+              swipe={swipe}
+              key={person.img}
+              person={person}
+              isFirst={isFirst}
+              zIndex={index * -1}
+              {...dragHandlers}
+            />
+          );
         })}
       </View>
     );
@@ -33,6 +127,7 @@ const Main = () => {
       <View paddingT-60>
         <Circles />
       </View>
+      <People />
     </View>
   );
 };
